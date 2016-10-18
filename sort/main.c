348caf2749585e8e9de2952888568a0320daadfd
Mark Kovalev <restonich@gmail.com>
2016-10-19 02:10:18 +0300

    Sorting program ver. 0.2.1
    Work in progress. I swear.

--- sort/main.c
+++ sort/main.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *x, int *y)
+void swap(char **x, char **y)
 {
-    int cur=*x;
+    char *cur=*x;
     *x=*y;
     *y=cur;
 }
 
-void bubble_sort(int *a, int n)
+int comparator(char *a, char *b)
+{
+    int i=0;
+    while((a[i]!='\0')&&(b[i]!='\0'))
+    {
+        if(a[i]>b[i])
+            return 1;
+        if(a[i]<b[i])
+            return 0;
+        ++i;
+    }
+    if(a[i]=='\0')
+        return 1;
+    return 0;
+}
+
+void bubble_sort(char **a, int n)
 {
     int i, j;
     for(i=n; i>0; --i)
         for(j=n-1; j>n-i; --j)
             if(a[j-1]>a[j])
-                swap(a+j, a+j-1);
+                swap(&a[j], &a[j-1]);
 }
 
 void insert_sort(int *a, int n)
@@ -27,10 +43,12 @@ void insert_sort(int *a, int n)
 
 void quick_sort(int *a, int n)
 {
-    int left, right, p;
     if(n<2)
         return;
+
+    int left, right, p;
     p=a[n/2];
+
     for (left=0, right=n-1;; ++left, --right)
     {
         while(a[left]<p)
@@ -39,26 +57,49 @@ void quick_sort(int *a, int n)
             --right;
         if(left>=right)
             break;
-        swap(a+left, a+right);
+        swap(a+left,a+right);
     }
+
     quick_sort(a, left);
     quick_sort(a+left, n-left);
 }
 
 int main()
 {
-    int n, i, s_method;
-    int *a;
+    int n, i, j, s_method, cur, *str_len;
+    char **a;
+    FILE *inp = fopen("input.txt", "r");
+    fscanf(inp, "%d\n", &n);
 
-    printf("Size of array: ");
-    scanf("%d", &n);
-    a=(int*)malloc(sizeof(int)*n);
+    str_len=(int*)malloc(sizeof(int)*n);
+	for (i=0; i<n; ++i)
+		str_len[i] = 0;
+	i=0;
+	do
+    {
+		cur=fgetc(inp);
+		if ((cur!='\n')&&(cur!=EOF))
+			++str_len[i];
+		else
+			++i;
+	} while(cur!=EOF);
 
-    printf("Enter elements: ");
-    for(i=0; i<n; ++i)
-        scanf("%d", &a[i]);
+	a=(char**)malloc(sizeof(char*)*i);
+	rewind(inp);
+	fscanf(inp, "%d\n", &n);
 
-    printf("Sort method (1. Bubble, 2. Insert, 3. Quick): ");
+	for(j=0; j<i; ++j)
+	{
+		a[j]=(char*)malloc(++str_len[j]);
+		int k=0;
+		cur=fgetc(inp);
+		while((cur!='\n')&&(cur!=EOF))
+			a[j][k++] = cur;
+		a[j][k] = '\0';
+	}
+	fclose(inp);
+
+    printf("Please, choose sort method (1. Bubble, 2. Insert, 3. Quick): ");
     scanf("%d", &s_method);
     switch(s_method)
     {
@@ -79,9 +120,6 @@ int main()
         quick_sort(a, n);
     }
 
-    printf("Sorted array: ");
-    for(i=0; i<n; ++i)
-        printf("%d ", a[i]);
 
     free(a);
     return 0;
